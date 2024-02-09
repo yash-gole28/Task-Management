8a5ec7bfb0ea8e98acc510ca4bf86ba07a7b7673
yash-gole28 <yashgole2806@gmail.com>
2024-02-09 17:01:02 +0530

    updates

--- Task-management/task/src/Components/AssignTask.jsx
+++ Task-management/task/src/Components/AssignTask.jsx
@@ -1,29 +1,57 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useCallback, useContext, useEffect, useState } from 'react'
 import { AuthContext } from './Context/AuthContext'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import toast from 'react-hot-toast'
 
 const AssignTask = () => {
     const {state} = useContext(AuthContext)
-    const[data , setData] = useState({name:"" ,category:"", description:"", date:"", priority:""})
+    const[data , setData] = useState({name:"" ,Description:"", DueDate:"", Priority:""})
+    const navigate = useNavigate()
    
     
-    function handleChange(event){
-        
-    }
-    useEffect(()=>{
-        if(state?.user){
-            console.log(state)
-            const newData = {...data,userId:`${state.user.id}`}
-            console.log(newData)
-           
+    const handleChange = useCallback((event)=>{
+      setData({...data,[event.target.name]:event.target.value})
+      console.log(data)
+  },[setData , data])
+
+
+
+
+    async function handleSubmit(event){
+      event.preventDefault()
+      try{
+       if(state?.user?.type === "admin"){
+        const response = await axios.post('http://localhost:8000/api/v1/tasks/task-assign',{name:data.name, Description:data.Description, DueDate:data.DueDate, Priority:data.Priority ,adminId:state?.user?.id})
+        if(response.data.success){
+          toast.success("task assigned successfully")
         }
       
-    },[])
+      }else{
+        toast.error('you are not allowed to assign task')
+       }
+      }catch(error){
+        console.log(error)
+      }
+    }
+    useEffect(()=>{
+        if(state?.user?.type === "admin"){
+            // const newData = {...data,userId:`${state.user.id}`}
+        }
+    },[state])
   return (
     <>
-     <form action="">
-        <input type="text" name='name' onChange={handleChange} />    
-        <button onClick={()=>console.log(data)}>data</button>
-    </form> 
+    <form onSubmit={handleSubmit}>
+        <label htmlFor="">Description</label>
+        <input type="text" name='Description' onChange={handleChange}/><br />
+        <label htmlFor="">Priority</label>
+        <input type="text" name='Priority' onChange={handleChange}/><br />
+        <label htmlFor="">DueDate</label>
+        <input type="date" name='DueDate' onChange={handleChange}/><br />
+        <label htmlFor="">Name</label>
+        <input type="text" name='name' onChange={handleChange}/>
+        <button type="submit">Update</button>
+      </form>
     </>
   )
 }
